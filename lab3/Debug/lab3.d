ff4520bedc0c08ee46ff51ea329ea37e1e652fd7
Lem0z3n <91253554+Lem0z3n@users.noreply.github.com>
2024-10-05 17:44:20 +0200

    Add files via upload

---
+++ lab3/Debug/lab3.d
@@ -0,0 +1,11 @@
+lab3.d lab3.o: ../lab3.c \
+ D:\Uni\CUARTOYERASMUS\PSyD\PSyD\labs\BSP\include/common_types.h \
+ ../systemLab3.h ../segs.h ../leds.h
+
+D:\Uni\CUARTOYERASMUS\PSyD\PSyD\labs\BSP\include/common_types.h:
+
+../systemLab3.h:
+
+../segs.h:
+
+../leds.h:
