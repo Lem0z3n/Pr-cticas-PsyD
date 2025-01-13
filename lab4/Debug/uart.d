11e3e8a246aed338e7e7df6b5f1f54604a4a02dd
Lem0z3n <91253554+Lem0z3n@users.noreply.github.com>
2025-01-13 10:44:58 +0100

    Delete lab4 directory

--- lab4/Debug/uart.d
+++
@@ -1,8 +0,0 @@
-uart.d uart.o: ../uart.c ..\..\BSP\include/s3c44b0x.h \
- ..\..\BSP\include/common_types.h ..\..\BSP\include/uart.h
-
-..\..\BSP\include/s3c44b0x.h:
-
-..\..\BSP\include/common_types.h:
-
-..\..\BSP\include/uart.h:
